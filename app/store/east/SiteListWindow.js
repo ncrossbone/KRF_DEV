f37f2a2eeb977638e82c64425950923aba91a20e
YJH <YJH@YJH-PC>
2015-09-14 15:06:14 +0900

    new

---
+++ app/store/east/SiteListWindow.js
@@ -0,0 +1,69 @@
+Ext.define('KRF_DEV.store.east.SiteListWindow', {
+	
+	extend: 'Ext.data.Store',
+
+	fields: ['id', 'name'],
+
+	autoLoad: true,
+
+	remoteSort: true,
+
+	listeners: {
+		beforeload: function(store) {
+			alert('111');
+			var queryTask = new esri.tasks.QueryTask('http://112.217.167.123:6080/arcgis/rest/services/reach/MapServer'); // 레이어 URL
+			var query = new esri.tasks.Query();
+			query.returnGeometry = false;
+			query.where = "권역 = '한강권역'";
+			query.outFields = ["*"];
+			
+			queryTask.execute(query, function(result){
+				var jsonStr = "[";
+				
+				Ext.each(result.layers, function(objLayer, idx, objLayers){
+					// 상위 node일때
+					if(objLayer.parentLayerId == -1){
+						jsonStr += "{\n";
+						jsonStr += "	\"id\": \"" + objLayer.id + "\",\n";
+						jsonStr += "	\"text\": \"" + objLayer.name + "\",\n";
+						jsonStr += "	\"cls\": "+'"'+"khLee-x-tree-node-text-bold"+'"'+",\n";
+						jsonStr += "	\"iconCls\": 'layerNoneImg',\n";
+						jsonStr += "	\"checked\": true,\n";
+						
+						// children node가 있을때
+						if(objLayer.subLayerIds != null){
+							jsonStr += "	\"expanded\": true,\n"; // 펼치기..
+							jsonStr += "\n	\"children\": [";
+							for(i = 0; i < objLayer.subLayerIds.length; i++){
+								jsonStr += "{\n";
+								jsonStr += "		\"id\": \"" + objLayers[objLayer.subLayerIds[i]].id + "\",\n";
+								jsonStr += "		\"text\": \"" + objLayers[objLayer.subLayerIds[i]].name + "\",\n";
+								jsonStr += "		\"cls\": "+'"'+"khLee-x-tree-node-text-small-nounder"+'"'+",\n";
+								jsonStr += "		\"iconCls\": "+'"'+"layerIconSize layer"+objLayers[objLayer.subLayerIds[i]].id+'"'+",\n";
+								jsonStr += "		\"leaf\": true,\n";
+								jsonStr += "		\"checked\": true\n";
+								jsonStr += "	}, ";
+								if(i == objLayer.subLayerIds.length - 1){
+									jsonStr = jsonStr.substring(0, jsonStr.length - 2); // 마지막에 "," 빼기
+									jsonStr += "]\n}, ";
+								}
+							}
+						}
+						// children node가 없을때
+						else{
+							jsonStr += "	\"leaf\": true"; 
+							jsonStr += "\n}, "
+						}
+					}
+					
+				});
+				
+				jsonStr = jsonStr.substring(0, jsonStr.length - 2); // 마지막에 "," 빼기
+				jsonStr += "]";
+			
+			
+				store.setData(jsonStr);
+	        });
+	    }
+	}
+});
