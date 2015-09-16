770158c33040b23616dde805d69134c1a63e6124
khLee <ncrossbone@gmail.com>
2015-09-16 19:05:56 +0900

    khLee - window 기능추가 - restore, maximize, minimize

--- app/view/common/WindowControl.js
+++ app/view/common/WindowControl.js
@@ -38,60 +38,6 @@ Ext.define('KRF_DEV.view.common.WindowControl', {
 		
 		var toolCtl = [];
 		
-		// 이전크기 툴 추가
-		if(this.restore == true){
-			
-			toolCtl.push({
-				type: 'restore',
-				hidden: true,
-				handler: function(evt, toolEl, owner, tool){
-					
-					var window = owner.up('window');
-					
-					if(window != undefined && window.tools != undefined && window.tools.length > 0){
-					
-						for(var i = 0; i < window.tools.length; i++){
-							if(window.tools[i].type == "restore"){
-								window.tools[i].setHidden(true);
-							}
-							if(window.tools[i].type == "minimize"){
-								window.tools[i].setHidden(false);
-							}
-							if(window.tools[i].type == "maximize"){
-								window.tools[i].setHidden(false);
-							}
-						}
-						
-						if(me.winSizeMode == "minimize"){
-							window.expand('', false);
-						}
-						
-						me.winSizeMode = "normal";
-						
-						window.setWidth(me.preWidth);
-						window.setHeight(me.preHeight);
-						window.setX(me.preX);
-						window.setY(me.preY);
-						
-						//console.info(me.items.items[0]);
-						
-						// window 아이템 resize
-						if(me.items.items != undefined && me.items.items.length > 0){
-							for(var i = 0; i < me.items.items.length; i++){
-								if(me.items.items[i].autoReSize == true){
-									me.items.items[i].setWidth(me.preWidth);
-									me.items.items[i].setHeight(me.preWidth);
-								}
-							}
-						}
-						
-					}
-					
-				}
-			});
-			
-		}
-		
 		// 최소화 툴 추가
 		if(this.minimize == true){
 			
@@ -136,41 +82,127 @@ Ext.define('KRF_DEV.view.common.WindowControl', {
 			
 		}
 		
+		// 이전크기 툴 추가
+		if(this.restore == true){
+			
+			toolCtl.push({
+				type: 'restore',
+				hidden: true,
+				handler: function(evt, toolEl, owner, tool){
+					
+					var window = owner.up('window');
+					
+					if(window != undefined && window.tools != undefined && window.tools.length > 0){
+					
+						for(var i = 0; i < window.tools.length; i++){
+							if(window.tools[i].type == "restore"){
+								window.tools[i].setHidden(true);
+							}
+							if(window.tools[i].type == "minimize"){
+								window.tools[i].setHidden(false);
+							}
+							if(window.tools[i].type == "maximize"){
+								window.tools[i].setHidden(false);
+							}
+						}
+						
+						if(me.winSizeMode == "minimize"){
+							window.expand('', false);
+						}
+						
+						me.winSizeMode = "normal";
+						
+						window.setWidth(me.preWidth);
+						window.setHeight(me.preHeight);
+						window.setX(me.preX);
+						window.setY(me.preY);
+						
+						// 아이템 리사이즈
+						me.setItemResize(me.items.items);
+						
+					}
+					
+				}
+			});
+			
+		}
+		
 		// 최대화 툴 추가
 		if(this.maximize == true){
 			
 			toolCtl.push({
 				type: 'maximize',
 				handler: function(evt, toolEl, owner, tool){
-					console.info(this.type);
-					return;
 					
 					var window = owner.up('window');
-
-					if(windowMode == "normal"){
-						windowWidth = window.getWidth();
-						windowHeight = window.getHeight();
-						windowX = window.getX();
-						windowY = window.getY();
+					
+					if(window != undefined && window.tools != undefined && window.tools.length > 0){
+						
+						for(var i = 0; i < window.tools.length; i++){
+							if(window.tools[i].type == "restore"){
+								window.tools[i].setHidden(false);
+							}
+							if(window.tools[i].type == "minimize"){
+								window.tools[i].setHidden(false);
+							}
+							if(window.tools[i].type == "maximize"){
+								window.tools[i].setHidden(true);
+							}
+						}
+						
+						if(me.winSizeMode == "normal"){
+							me.preWidth = window.getWidth();
+							me.preHeight = window.getHeight();
+							me.preX = window.getX();
+							me.preY = window.getY();
+						}
+						
+						me.winSizeMode = "maximize";
+						
+						var centerContainer = KRF_DEV.getApplication().centerContainer;
+						
+						if(me.winSizeMode == "minimize"){
+							window.expand('', false);
+						}
+						
+						window.setWidth(centerContainer.getWidth());
+						window.setHeight(centerContainer.getHeight() - centerContainer.header.getHeight());
+						window.setX(centerContainer.getX());
+						window.setY(centerContainer.getY() + centerContainer.header.getHeight());
+						
+						// 아이템 리사이즈
+						me.setItemResize(me.items.items);
+						
 					}
-					
-					windowMode = "maximize";
-					
-					console.info(this);
-					
-					window.expand('', false);
-					window.setWidth(center.getWidth());
-					window.setHeight(center.getHeight());
-					window.setX(centerX);
-					window.setY(centerY);
-					
-					//window.alignTo('center-panel', 'tl-br');
 				}
 			});
 			
 		}
 		
 		this.tools = toolCtl;
+	},
+
+	// 내부 아이템 리사이즈
+	setItemResize: function(items){
+		
+		var me = this;
+		
+		if(items != undefined && items.length > 0){
+			
+			for(var i = 0; i < items.length; i++){
+				
+				// autoReSize option이 true일 경우만..
+				if(items[i].autoReSize == true){
+					
+					items[i].setWidth(me.preWidth);
+					items[i].setHeight(me.preWidth);
+					
+				}
+				
+			}
+			
+		}
+		
 	}
 		
 });
