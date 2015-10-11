c7f8e90f8c7c8598c9bf1cc82f2849da9e1533d0
khLee <ncrossbone@gmail.com>
2015-10-11 13:59:11 +0900

    khLee - 20151011 add

---
+++ app/view/south/SearchResultWindow.js
@@ -0,0 +1,27 @@
+Ext.define('KRF_DEV.view.south.SearchResultWindow', {
+	
+	extend : 'Ext.window.Window',
+	//xtype : 'common-windowcontrol',
+	width: 300,
+	height: 300,
+	//renderTo: Ext.getBody(),
+	
+	items: [{
+		xtype: 'tabpanel',
+		header: false,
+		activeTab: 0,
+		title: 'dfa',
+		items: [{
+			id: 'tab1',
+			title: 'tab1',
+			items: [{
+				xtype: 'grid',
+				title: 'girdtest'
+			}, {
+				xtype: 'grid',
+				title: 'ddd'
+			}]
+		}]
+	}]
+		
+});
