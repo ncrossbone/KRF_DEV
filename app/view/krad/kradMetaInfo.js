bf09c4029032eed58541b0b4f9ab4e0a309f5d08
pdj <ncrossbone@gmail.com>
2016-10-27 18:59:49 +0900

    pdj - meta (html)

--- app/view/krad/kradMetaInfo.js
+++ app/view/krad/kradMetaInfo.js
@@ -7,136 +7,131 @@ Ext.define('KRF_DEV.view.krad.kradMetaInfo', {
 	title: '메타상세정보',
 	header: true,
     frame: true,
-	width: 400,
-	height: 600,
-	
+	width: 470,
+	height: 559,
 	x: 687,
 	y: 226,
 
 	plain: true, // 요게 있어야 background: transparent 먹음..
 	//cls: 'dj_toolbarConf',
-	style: "border: 0px;",
-	
-	
-
 	
 	items:[{
 		xtype : 'container',
-		width: '400px',
-		height: '100%',
-		border: true,
+		width: 470,
+		height: 559,
 		items:[{
-			xtype: 'container',
-			layout:{
-				xtype:'vbox'
-			},
-			items:[{
-				xtype:'label',
-				text:'기본정보'
-			},{
-				xtype: 'container',
-				layout : {
-					type : 'vbox'
-				},
-				items:[{
-					xtype:'textfield',
-					fieldLabel:'지도명'					
-				},{
-					xtype:'container',
-					layout:{
-						type: 'hbox'
-					},
-					items:[{
-						xtype:'textfield',
-						fieldLabel:'도엽명',
-						width:'130px'
-					},{
-						xtype:'textfield',
-						fieldLabel:'도엽변호',
-						width:'130px'
-					},{
-						xtype:'textfield',
-						fieldLabel:'축척',
-						width:'130px'
-					}]
-					
-				},{
-					xtype:'textfield',
-					fieldLabel:'제작/갱신일자'
-				},{
-					xtype:'textfield',
-					fieldLabel:'자료포맷명'
-				},{
-					xtype:'textfield',
-					fieldLabel:'관리/배포기관'
-				},{
-					xtype:'textfield',
-					fieldLabel:'홈페이지'
-				},{
-					xtype:'textfield',
-					fieldLabel:'책임기관명'
-				},{
-					xtype:'textfield',
-					fieldLabel:'요약설명'
-				}]
-
-			},{
-				xtype:'label',
-				text:'기준계/공간정보'
-			},{
-				xtype: 'container',
-				layout : {
-					type : 'vbox'
-				},
-				items:[{
-					xtype:'textfield',
-					fieldLabel:'지도명'					
-				},{
-					xtype:'container',
-					layout:{
-						type: 'hbox'
-					},
-					items:[{
-						xtype:'textfield',
-						fieldLabel:'도엽명',
-						width:'130px'
-					},{
-						xtype:'textfield',
-						fieldLabel:'도엽변호',
-						width:'130px'
-					},{
-						xtype:'textfield',
-						fieldLabel:'축척',
-						width:'130px'
-					}]
-					
-				},{
-					xtype:'textfield',
-					fieldLabel:'제작/갱신일자'
-				},{
-					xtype:'textfield',
-					fieldLabel:'자료포맷명'
-				},{
-					xtype:'textfield',
-					fieldLabel:'관리/배포기관'
-				},{
-					xtype:'textfield',
-					fieldLabel:'홈페이지'
-				},{
-					xtype:'textfield',
-					fieldLabel:'책임기관명'
-				},{
-					xtype:'textfield',
-					fieldLabel:'요약설명'
-				}]
-
-			}]
-		
+			html: '<!doctype html>																																																						'+	
+			'<html lang=\"ko\">                                                                                                           '+
+			'<head>                                                                                                                     '+
+			'<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />                                                      '+
+			'<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge, chrome=1\" />                                                          '+
+			'<title>KRF-TOOLTIP</title>                                                                                                 '+
+			'<!--[if lt ie 9]>                                                                                                          '+
+			'<script src=\"js/html5shiv.js\"></script>                                                                                    '+
+			'<![endif]-->                                                                                                               '+
+			'<link href=\"./resources/css/BasicSet.css\" rel=\"stylesheet\" type=\"text/css\" />                                                          '+
+			'<style type=\"text/css\">                                                                                                    '+
+			'table.meta { width: 450px; border-top: 2px solid #366be4; }                                                 '+
+			'table.meta> tbody> tr> th { background: #f3f3f2; font-weight: bold; text-align: left !important;  }                                                     '+
+			'table.meta> tbody> tr> td { }                                                                                              '+
+			'table.meta> tbody> tr> th, table.meta> tbody> tr> td { padding: 10px 0px 10px 10px; border-bottom: 1px solid #d4d1cc; font-size: 12px!important; }    '+
+			'</style>                                                                                                                   '+
+			'</head>                                                                                                                    '+
+			'<body>                                                                                                                     '+
+			'<table class=\"meta\" summary=\"\">                                                                                            '+
+			'	<colgroup>                                                                                                                '+
+			'    	<col width=\"110\" />                                                                                                   '+
+			'        <col />                                                                                                            '+
+			'        <col />                                                                                                            '+
+			'        <col />                                                                                                            '+
+			'        <col />                                                                                                            '+
+			'        <col />                                                                                                            '+
+			'    </colgroup>                                                                                                            '+
+			'    <tbody>                                                                                                                '+
+			'    	<tr>                                                                                                                  '+
+			'        	<th>지도명</th>                                                                                                      '+
+			'            <td colspan=\"5\">대분류토지피복지도 : 추가</td>                                                                            '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>도엽명</th>                                                                                                      '+
+			'            <td>추자</td>                                                                                                    '+
+			'            <th>도엽번호</th>                                                                                                  '+
+			'            <td>33602</td>                                                                                                 '+
+			'            <th>축척</th>                                                                                                    '+
+			'            <td>1:50,000</td>                                                                                              '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>제작/갱신일자</th>                                                                                                  '+
+			'            <td colspan=\"5\">20010725</td>                                                                                  '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>자료포맷명</th>                                                                                                    '+
+			'            <td colspan=\"5\">TIF</td>                                                                                       '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>관리/배포기관</th>                                                                                                  '+
+			'            <td colspan=\"5\">환경부 정보화담당관실(044-201-6422)</td>                                                                 '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>홈페이지</th>                                                                                                     '+
+			'            <td colspan=\"5\">http://egis.me.go.kr</td>                                                                      '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>책임기관명</th>                                                                                                    '+
+			'            <td colspan=\"5\">환경부 정보화담당관실(044-201-6422, biolmj@korea.kr)</td>                                                '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>요약설명</th>                                                                                                     '+
+			'            <td colspan=\"5\">환경부에서 2001년 제작된 토지피복도</td>                                                                     '+
+			'        </tr>                                                                                                              '+
+			'    </tbody>                                                                                                               '+
+			'</table>                                                                                                                   '+
+			'                                                                                                                           '+
+			'<br />                                                                                                                     '+
+			'                                                                                                                           '+
+			'                                                                                                                           '+
+			'<table class=\"meta\" summary=\"\">                                                                                            '+
+			'	<colgroup>                                                                                                                '+
+			'    	<col width=\"110\" />                                                                                                   '+
+			'        <col />                                                                                                            '+
+			'        <col />                                                                                                            '+
+			'        <col />                                                                                                            '+
+			'    </colgroup>                                                                                                            '+
+			'    <tbody>                                                                                                                '+
+			'    	<tr>                                                                                                                  '+
+			'        	<th>기준계명칭</th>                                                                                                    '+
+			'            <td colspan=\"5\">평명직각좌표계(TM)(중부원점)</td>                                                                         '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>기준타원체</th>                                                                                                    '+
+			'            <td>Bessel</td>                                                                                                '+
+			'            <th>기준좌표계</th>                                                                                                 '+
+			'            <td>측지학적기준계</td>                                                                                               '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>동쪽경계</th>                                                                                                     '+
+			'            <td>126.2480291</td>                                                                                           '+
+			'            <th>서쪽경계</th>                                                                                                  '+
+			'            <td>126.4980058</td>                                                                                           '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>남쪽경계</th>                                                                                                     '+
+			'            <td>33.7532267</td>                                                                                            '+
+			'            <th>북쪽경계</th>                                                                                                  '+
+			'            <td>34.0031973</td>                                                                                            '+
+			'        </tr>                                                                                                              '+
+			'        <tr>                                                                                                               '+
+			'        	<th>행정지역범위</th>                                                                                                   '+
+			'            <td colspan=\"3\"></td>                                                                                          '+
+			'        </tr>                                                                                                              '+
+			'</table>                                                                                                                   '+
+			'</body>                                                                                                                    '+
+			'</html>                                                                                                                    '
 			
 		}]
 	}],
 	initComponent: function(){
-		
+		console.info(this);
 		//store : Ext.create('KRF_DEV.store.krad.krad_tmp'),
 		
 		//localStorage['_kradGroupInfo_']
--- app/view/krad/kradSchConf.js
+++ app/view/krad/kradSchConf.js
@@ -16,114 +16,128 @@ Ext.define('KRF_DEV.view.krad.kradSchConf', {
 	//cls: 'dj_toolbarConf',
 	style: "border: 1px;",
 	header: false,
-	
-	layout: {
-		type: 'accordion'
-	},	
+	layout:{
+		type:'vbox'
+	},
 	items:[{
-		xtype : 'panel',
-		//autoScroll: true,
-		layout : {
-			type : 'vbox'
+		layout: {
+			type: 'accordion'
 		},
-		cls: 'dj_layer_nm',
-		title :  '공통',
-		
+		width: 300,
 		items:[{
-			xtype: 'container',
-			width: '100%',
-			height: '100%',
-			//id: 'krad_grid',
-			items: [{
-				xtype: 'grid',
-				store : Ext.create('KRF_DEV.store.krad.krad_tmp'),
-				columns: [{
-					text:'체크박스',
-					width: 50,
-					renderer:function(value){
-						value = 0;
-						if (value == "true") {
-							value = 1;
-						}
-						return "<input type='checkbox'" + (value ? "checked='checked'" : "") + ">";
-					}
-					},{	 
-					text      : '측정망명',
-					dataIndex : 'kradCommonNm',
-					width: 150
-					//filter: {type: 'numeric'}
-					},{	 
-						text:'버튼',
-						align:'center',
-						xtype:'actioncolumn',
-						width:50,
-						items:[{
-							xtype:'button',
-							text:'button',
-							handler: function(a,b,c,d){
-								var metaInfo = Ext.getCmp("kradMetaInfo");
-								if(metaInfo == undefined){
-									metaInfo = Ext.create("KRF_DEV.view.krad.kradMetaInfo");
-								}
-								metaInfo.show();
+			xtype : 'panel',
+			//autoScroll: true,
+			layout : {
+				type : 'vbox'
+			},
+			cls: 'dj_layer_nm',
+			title :  '공통',
+			items:[{
+				xtype: 'container',
+				width: '100%',
+				height: '100%',
+				hideHeaders: true,
+				//id: 'krad_grid',
+				items: [{
+					xtype: 'grid',
+					store : Ext.create('KRF_DEV.store.krad.krad_tmp'),
+					columns: [{
+						text:'체크박스',
+						width: 50,
+						renderer:function(value,b,c,d){
+							console.info(b);
+							console.info(c);
+							console.info(d);
+							value = 0;
+							if (value == "true") {
+								value = 1;
 							}
+							return "<input type='checkbox'" + (value ? "checked='checked'" : "") + ">";
+						}
+						},{	 
+						text      : '측정망명',
+						dataIndex : 'kradCommonNm',
+						width: 150
+						//filter: {type: 'numeric'}
+						},{	 
+							text:'버튼',
+							align:'center',
+							xtype:'actioncolumn',
+							width:50,
+							items:[{
+								xtype:'button',
+								text:'button',
+								handler: function(a,b,c,d){
+									var metaInfo = Ext.getCmp("kradMetaInfo");
+									if(metaInfo == undefined){
+										metaInfo = Ext.create("KRF_DEV.view.krad.kradMetaInfo",{
+											asdf: 'asdf'
+										});
+									}
+									metaInfo.show();
+								}
+							}]
 						}]
 					}]
 				}]
-			}]
-		},{
-
-		xtype : 'panel',
-		//autoScroll: true,
-		layout : {
-			type : 'vbox'
-		},
-		cls: 'dj_layer_nm',
-		title :  '공통',
-		
-		items:[{
-			xtype: 'container',
-			width: '100%',
-			height: '100%',
-			//id: 'krad_grid',
-			items: [{
-				xtype: 'grid',
-				store : Ext.create('KRF_DEV.store.krad.krad_tmp2'),
-				columns: [{
-					text:'체크박스',
-					width: 50,
-					renderer:function(value){
-						value = 0;
-						if (value == "true") {
-							value = 1;
-						}
-						return "<input type='checkbox'" + (value ? "checked='checked'" : "") + ">";
-					}
-					},{	 
-					text      : '측정망명',
-					dataIndex : 'kradCommonNm',
-					width: 150
-					//filter: {type: 'numeric'}
-					},{	 
-						text:'사용자 지정',
-						align:'center',
-						xtype:'actioncolumn',
-						width:50,
-						items:[{
-							xtype:'button',
-							text:'button',
-							handler: function(a,b,c,d){
-								var metaInfo = Ext.getCmp("kradMetaInfo");
-								if(metaInfo == undefined){
-									metaInfo = Ext.create("KRF_DEV.view.krad.kradMetaInfo");
+			},{
+				xtype : 'panel',
+				//autoScroll: true,
+				layout : {
+					type : 'vbox'
+				},
+				cls: 'dj_layer_nm',
+				title :  '사용자 지정',
+				items:[{
+					xtype: 'container',
+					width: '100%',
+					height: '100%',
+					//id: 'krad_grid',
+					items: [{
+						xtype: 'grid',
+						store : Ext.create('KRF_DEV.store.krad.krad_tmp2'),
+						columns: [{
+							text:'체크박스',
+							width: 50,
+							renderer:function(value){
+								value = 0;
+								if (value == "true") {
+									value = 1;
 								}
-								metaInfo.show();
+								return "<input type='checkbox'" + (value ? "checked='checked'" : "") + ">";
 							}
+							},{	 
+							text      : '측정망명',
+							dataIndex : 'kradCommonNm',
+							width: 150
+							//filter: {type: 'numeric'}
+							},{	 
+								text:'버튼',
+								align:'center',
+								xtype:'actioncolumn',
+								width:50,
+								items:[{
+									xtype:'button',
+									text:'button',
+									handler: function(a,b,c,d){
+										var metaInfo = Ext.getCmp("kradMetaInfo");
+										if(metaInfo == undefined){
+											metaInfo = Ext.create("KRF_DEV.view.krad.kradMetaInfo");
+										}
+										metaInfo.show();
+									}
+								}]
+							}]
 						}]
 					}]
-				}]
+				
+				
 			}]
-		}],
+	},{
+		xtype:'button',
+		text: '적용'
+	}]
+	,
 	initComponent: function(){
 		
 		//store : Ext.create('KRF_DEV.store.krad.krad_tmp'),
