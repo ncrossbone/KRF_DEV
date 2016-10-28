2a4649721c4e8a7745701eae766e891f018ea3fd
khLee <ncrossbone@gmail.com>
2016-10-28 09:56:18 +0900

    khLee - tmp

--- app/view/map/KRADLayerAdmin.js
+++ app/view/map/KRADLayerAdmin.js
@@ -15,6 +15,11 @@ Ext.define("KRF_DEV.view.map.KRADLayerAdmin", {
 	rchMouseEvt: null,
 	mousSymbol: null,
 	rchIds: null,
+	stRchId: null,
+	edRchId: null,
+	btnId: null,
+	stExtDataId: null,
+	edExtDataId: null,
 	
 	/* Point Symbol */
 	basicSymbol_P: null,
@@ -67,9 +72,13 @@ Ext.define("KRF_DEV.view.map.KRADLayerAdmin", {
 		/* khLee Test 임시 설정 개발완료 후 삭제할것.. 끝 */
 		
 		require(["esri/symbols/SimpleMarkerSymbol",
+		         "esri/symbols/SimpleLineSymbol",
+		         "esri/symbols/SimpleFillSymbol",
 		         "dojo/_base/Color",
 		         "esri/layers/GraphicsLayer"],
 				function(SimpleMarkerSymbol,
+						SimpleLineSymbol,
+						SimpleFillSymbol,
 						Color,
 						GraphicsLayer){
 			
@@ -93,6 +102,10 @@ Ext.define("KRF_DEV.view.map.KRADLayerAdmin", {
 			me.drawSymbol_P.setStyle(SimpleMarkerSymbol.STYLE_CIRCLE);
 			me.drawSymbol_P.setSize(10);
 			me.drawSymbol_P.setColor(new Color([255,0,0,1]));
+			
+			me.drawSymbol_L = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([0, 255, 0]), 5);
+			
+			me.drawSymbol_A = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([0, 0, 0]), 2), new Color([0, 0, 255, 0.3]));
 			/* 심볼 설정 끝 */
 			
 			me.kradPointLayer = new GraphicsLayer();
@@ -113,9 +126,11 @@ Ext.define("KRF_DEV.view.map.KRADLayerAdmin", {
     	
     	if(me.drawOption == "startPoint"){
     		me.mousSymbol = coreMap.reachLayerAdmin_v3_New.startSymbol;
+    		me.btnId = "btnMenu04";
     	}
-    	if(me.drawOption == "startPoint"){
+    	if(me.drawOption == "endPoint"){
     		me.mousSymbol = coreMap.reachLayerAdmin_v3_New.endSymbol;
+    		me.btnId = "btnMenu05";
     	}
     	
     	var where = "";
@@ -167,6 +182,7 @@ Ext.define("KRF_DEV.view.map.KRADLayerAdmin", {
 	    	
 	    	me.onMouseOver(evtType);
 	    	me.onMouseOut(evtType);
+	    	me.onMouseClick(evtType);
     	});
     },
     drawGraphic: function(featureSet, evtType){
@@ -241,20 +257,110 @@ Ext.define("KRF_DEV.view.map.KRADLayerAdmin", {
 				function(on){
     		
     		me.mouseClickObj_P = on(me.kradPointLayer, "click", function(evt){
+	    		console.info(evt);
+	    		if(me.drawOption == "startPoint"){
+	    			me.stRchId = evt.graphic.attributes.RCH_ID;
+	    			me.stExtDataId = evt.graphic.attributes.EXT_DATA_ID;
+	    			me.stEventOrder = evt.graphic.attributes.EVENT_ORDER;
+	    		}
+	    		if(me.drawOption == "endPoint"){
+	    			me.edRchId = evt.graphic.attributes.RCH_ID;
+	    			me.edExtDataId = evt.graphic.attributes.EXT_DATA_ID;
+	    			me.edEventOrder = evt.graphic.attributes.EVENT_ORDER;
+	    		}
 	    		
-		    	coreMap.reachLayerAdmin_v3_New.drawEnd(btnId);
+		    	coreMap.reachLayerAdmin_v3_New.drawEnd(me.btnId);
 				coreMap.reachLayerAdmin_v3_New.drawSymbol(evt.graphic.geometry, me.mousSymbol, me.drawOption); // 심볼 그리기
 				coreMap.reachLayerAdmin_v3_New.selectLineWithWhere(where, me.drawOption); // 라인 검색
 				
-				kradPELayer_mouseout.remove();
-				kradPELayer_mouseover.remove();
-				kradPELayer_click.remove();
+				me.mouseOverObj_P.remove();
+				me.mouseOutObj_P.remove();
+				me.mouseClickObj_P.remove();
 				
-				evt.graphic.setSymbol(outSymbol);
-				kradPELayer.setVisibility(false);
+				me.kradPointLayer.clear();
 				
-				me.close();
+				var popCtl = Ext.getCmp("kradEvtPop");
+				if(popCtl != undefined){
+					popCtl.close();
+				}
 	    	});
+    	});
+    },
+    drawKRADLayer: function(){
+    	
+    	this.drawKRADrchLayer(me.stRchId, me.stExtDataId, me.stEventOrder);
+    	this.drawKRADrchLayer(me.edRchId, me.edExtDataId, me.edEventOrder);
+    },
+    drawKRADrchLayer: function(rchId, extDataId, eventOrder){
+    	
+    	var me = this;
+    	var coreMap = GetCoreMap();
+    	
+    	require(["esri/tasks/QueryTask",
+    	         "esri/tasks/query"],
+    	         function(QueryTask,
+    	        		 Query){
+    		
+	    	var comDownRchId = coreMap.reachLayerAdmin_v3_New.grpCommDownLine.attributes.RCH_ID;
+	    	
+	    	if(rchId != null && extDataId != null){
+	    		
+	    		for(var i = 0; i < me.kradInfo.length; i++){
+	    			
+	    			if(extDataId == me.kradInfo[i].EXT_DATA_ID){
+	    				
+	    				var leLayerId = me.kradInfo[i].LE_LAYER_ID;
+	    				var aeLayerId = me.kradInfo[i].AE_LAYER_ID;
+	    				
+	    				var where = "RCH_ID = '" + rchId + "' AND EXT_DATA_ID = '" + extDataId + "'";
+	    				
+	    				/***** 데이터쪽 EVENT_ORDER 넣어달라고 부탁하기.. *****/
+	    				if(eventOrder != null && eventOrder != undefined){
+	    					
+		    				if(rchId == comDownRchId){
+		    					
+		    					where += " AND EVENT_ORDER <= " + eventOrder;
+		    				}
+		    				else{
+		    					
+		    					where += " AND EVENT_ORDER > " + eventOrder;
+		    				}
+	    				}
+	    				
+	    				var queryTaskLE = new esri.tasks.QueryTask(_kradInfo.kradServiceUrl + "/" + leLayerId);
+	    				var queryLE = new esri.tasks.Query();
+	    				queryLE.returnGeometry = true;
+	    				queryLE.outFields = ["*"];
+	    				queryLE.where = where;
+	    				
+	    				queryTaskLE.execute(queryLE, function(featureSet){
+	    					
+	    					for(var fCnt = 0; fCnt < featureSet.features.length; fCnt++){
+	    						
+	    						var graphic = featureSet.features[fCnt];
+	    						graphic.setSymbol(me.drawSymbol_L);
+	    						coreMap.reachLayerAdmin_v3_New.addGraphics(graphic, "lineGrpLayer");
+	    					}
+	    				});
+	    				
+	    				var queryTaskAE = new esri.tasks.QueryTask(_kradInfo.kradServiceUrl + "/" + aeLayerId);
+	    				var queryAE = new esri.tasks.Query();
+	    				queryAE.returnGeometry = true;
+	    				queryAE.outFields = ["*"];
+	    				queryAE.where = where;
+	    				
+	    				queryTaskAE.execute(queryAE, function(featureSet){
+	    					
+	    					for(var fCnt = 0; fCnt < featureSet.features.length; fCnt++){
+	    						
+	    						var graphic = featureSet.features[fCnt];
+	    						graphic.setSymbol(me.drawSymbol_A);
+	    						coreMap.reachLayerAdmin_v3_New.addGraphics(graphic, "areaGrpLayer");
+	    					}
+	    				});
+	    			}
+	    		}
+	    	}
     	});
     }
 });
