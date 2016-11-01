dacaacb4d83d97b2f0b6e0c35d6e4d6ef64ef7b3
hyeokpark92 <hyeokpark92@gmail.com>
2016-11-01 14:31:48 +0900

    hyeok-commit

--- app/view/west/ButtonPanelController.js
+++ app/view/west/ButtonPanelController.js
@@ -310,6 +310,11 @@ Ext.define('KRF_DEV.view.west.ButtonPanelController', {
 			Ext.get('_mapDiv__gc').setStyle('cursor','url(./resources/images/symbol/btn_start01.png),auto');
 			KRF_DEV.getApplication().fireEvent('pointDrawClick', "point", el.id, false);
 			*/
+			var kradMetaInfo = Ext.getCmp("kradMetaInfo");
+			var kradSchConf = Ext.getCmp("kradSchConf");
+			
+			if(kradMetaInfo!=undefined) kradMetaInfo.close();
+			if(kradSchConf!=undefined) kradSchConf.close();
 		}
 		
 		////console.info("dd");
