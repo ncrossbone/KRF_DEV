9979c36fa52a4038c89cc72034baa773b402965d
pdj <ncrossbone@gmail.com>
2016-11-17 15:37:55 +0900

    pdj - tmp

--- resources/js/common.js
+++ resources/js/common.js
@@ -1698,7 +1698,6 @@ SetWestCollapseXY = function(option){
 	}
 	
 	var sConfig = Ext.getCmp("searchConfig");
-	
 	if(sConfig != undefined){
 		
 		if(sConfig.hidden == false){
@@ -1707,6 +1706,15 @@ SetWestCollapseXY = function(option){
 		}
 	}
 	
+	var shConfig = Ext.getCmp("searchConfigHeader");
+	if(shConfig != undefined){
+		
+		if(shConfig.hidden == false){
+			
+			shConfig.setX(shConfig.getX() + offsetWidth);
+		}
+	}
+	//searchConfigHeader
 	
 	//krad 리스트
 	var kConfig = Ext.getCmp("kradSchConf");
