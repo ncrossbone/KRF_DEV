a3d1557d209384b64180b5653fb798395d6e8de1
CHUL-PC\CHUL <pchul16@gmail.com>
2017-07-28 16:54:29 +0900

    검색부분 ajax 비동기로 변경

--- app/store/south/SearchResultGrid_D.js
+++ app/store/south/SearchResultGrid_D.js
@@ -239,7 +239,7 @@ Ext.define('KRF_DEV.store.south.SearchResultGrid_D', {
         		params: { WS_CD: WS_CD, AM_CD: AM_CD, AS_CD: AS_CD
         			, startYear: startYear, startMonth: startMonth, endYear: endYear, endMonth: endMonth
         			, ADM_CD: ADM_CD, siteIds: store.siteIds, firstSearch: firstSearch},
-        		async: false, // 비동기 = async: true, 동기 = async: false
+        		async: true, // 비동기 = async: true, 동기 = async: false
         		success : function(response, opts) {
         			store.startYear = startYear;
         			store.startMonth = startMonth;
