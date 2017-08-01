747ebfa8a576b7411fb672102ce08a8062457b40
CHUL-PC\CHUL <pchul16@gmail.com>
2017-08-01 13:11:03 +0900

    레포트 부분 _API 추가

--- report/app/view/map/rptCoreMap.js
+++ report/app/view/map/rptCoreMap.js
@@ -61,7 +61,7 @@ Ext.define('Report.view.map.rptCoreMap', {
 			me.printTask = Ext.create("KRF_DEV.view.map.task.CustomPrintTask",
         			me.map,
         			"_rptMapDiv_",
-        			"../resources/jsp/CustomPrintTask_New.jsp",
+        			'.'+_API.CustomPrintTask_New, //"../resources/jsp/CustomPrintTask_New.jsp",
         			"../resources/jsp/proxy.jsp",
         			//"./proxy.jsp",
         			_arcServiceUrl,
