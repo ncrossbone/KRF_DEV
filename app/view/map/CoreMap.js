8afdb51b844e4d92f49887e91588b3ccb769bf11
CHUL-PC\CHUL <pchul16@gmail.com>
2018-02-26 10:34:13 +0900

    위성영상 추가

--- app/view/map/CoreMap.js
+++ app/view/map/CoreMap.js
@@ -22,6 +22,9 @@ Ext.define('KRF_DEV.view.map.CoreMap', {
 	printTask:null,
 	baseMap: null,
 	
+	satelliteMap:null,
+	hybridMap:null,
+	
 	tmGraphicLayerCat: null, // 집수구역 단위 주제도 그래픽 레이어
 	tmLabelLayerCat: null, // 집수구역 단위 주제도 라벨 레이어
 	
@@ -31,6 +34,34 @@ Ext.define('KRF_DEV.view.map.CoreMap', {
 //	x: -378,
 //	y: -80,
 	
+	vworldTileInfo : new esri.layers.TileInfo({
+        rows: 256, cols: 256, dpi: 96,
+        origin: {x: -20037508.342787, y: 20037508.342787},
+        spatialReference: {wkid: 102100},
+        lods: [
+				{level:0, resolution:156543.033928,    scale:591657527.591555},
+				{level:1, resolution:78271.5169639999, scale:295828763.795777},
+				{level:2, resolution:39135.7584820001, scale:147914381.897889},
+				{level:3, resolution:19567.8792409999, scale:73957190.948944},
+				{level:4, resolution:9783.93962049996, scale:36978595.474472},
+				{level:5, resolution:4891.96981024998, scale:18489297.737236},
+				{level:6, resolution:2445.98490512499, scale:9244648.868618},
+				{level:7, resolution:1222.99245256249, scale:4622324.434309}, //start
+				{level:8, resolution:611.49622628138,  scale:2311162.217155},
+				{level:9, resolution:305.748113140558, scale:1155581.108577},
+				{level:10,resolution:152.874056570411, scale:577790.554289},
+				{level:11,resolution:76.4370282850732, scale:288895.277144},
+				{level:12,resolution:38.2185141425366, scale:144447.638572},
+				{level:13,resolution:19.1092570712683, scale:72223.819286},
+				{level:14,resolution:9.55462853563415, scale:36111.909643},
+				{level:15,resolution:4.77731426794937, scale:18055.954822},
+				{level:16,resolution:2.38865713397468, scale:9027.977411},
+				{level:17,resolution:1.19432856685505, scale:4513.988705},
+				{level:18,resolution:0.597164283559817,scale:2256.994353}, //end
+				{level:19,resolution:0.298582141647617,scale:1128.497176}
+          ]
+      }),
+      
 	initComponent: function() {
 		this.on('render', this.mapRendered, this);
 		this.callParent();
@@ -54,6 +85,21 @@ Ext.define('KRF_DEV.view.map.CoreMap', {
             
         	//me.map.resize();
         	me.baseMapInit();
+        	
+//        	me.baseMap.hide();
+        	
+        	me.initVWorldSatelliteLayer();
+    		me.initVWorldHybridLayer();
+    		
+    		me.satelliteMap = new VworldTiledSatelliteMapServiceLayer();
+    		me.hybridMap = new VworldTiledHybridMapServiceLayer();
+    		
+    		me.map.addLayer(me.satelliteMap);
+    		me.map.addLayer(me.hybridMap);
+    		
+    		me.satelliteMap.hide();
+    		me.hybridMap.hide();
+    	
         	me.map.setLevel(8);
 
         	window.clearInterval(timerId);
@@ -101,33 +147,7 @@ Ext.define('KRF_DEV.view.map.CoreMap', {
 		    constructor: function(opts) {
 		      opts = opts || {};
 		      this.spatialReference = new esri.SpatialReference({wkid: 102100});
-		      this.tileInfo = new esri.layers.TileInfo({
-		        rows: 256, cols: 256, dpi: 96,
-		        origin: {x: -20037508.342787, y: 20037508.342787},
-		        spatialReference: {wkid: 102100},
-		        lods: [
-						{level:0, resolution:156543.033928,    scale:591657527.591555},
-						{level:1, resolution:78271.5169639999, scale:295828763.795777},
-						{level:2, resolution:39135.7584820001, scale:147914381.897889},
-						{level:3, resolution:19567.8792409999, scale:73957190.948944},
-						{level:4, resolution:9783.93962049996, scale:36978595.474472},
-						{level:5, resolution:4891.96981024998, scale:18489297.737236},
-						{level:6, resolution:2445.98490512499, scale:9244648.868618},
-						{level:7, resolution:1222.99245256249, scale:4622324.434309}, //start
-						{level:8, resolution:611.49622628138,  scale:2311162.217155},
-						{level:9, resolution:305.748113140558, scale:1155581.108577},
-						{level:10,resolution:152.874056570411, scale:577790.554289},
-						{level:11,resolution:76.4370282850732, scale:288895.277144},
-						{level:12,resolution:38.2185141425366, scale:144447.638572},
-						{level:13,resolution:19.1092570712683, scale:72223.819286},
-						{level:14,resolution:9.55462853563415, scale:36111.909643},
-						{level:15,resolution:4.77731426794937, scale:18055.954822},
-						{level:16,resolution:2.38865713397468, scale:9027.977411},
-						{level:17,resolution:1.19432856685505, scale:4513.988705},
-						{level:18,resolution:0.597164283559817,scale:2256.994353}, //end
-						{level:19,resolution:0.298582141647617,scale:1128.497176}
-		          ]
-		      });
+		      this.tileInfo = me.vworldTileInfo;
 		      
 		      me.tileInfo = this.tileInfo;
 		      
@@ -140,8 +160,6 @@ Ext.define('KRF_DEV.view.map.CoreMap', {
 		        	  wkid: 102100
 		          }
 		      });
-		      
-		      
 		      
 		      var size = {
 					  width: window.innerWidth || document.body.clientWidth,
@@ -205,9 +223,52 @@ Ext.define('KRF_DEV.view.map.CoreMap', {
 		  });
 		me.baseMap = new CustomMapsLayer();
 		this.map.addLayer(me.baseMap);
-		
-		
-		
+	},
+	// 브이월드 위성맵 세팅
+	initVWorldSatelliteLayer:function () {
+		var me = this;
+		dojo.declare("VworldTiledSatelliteMapServiceLayer", esri.layers.TiledMapServiceLayer, 
+		{
+			constructor: function () {
+				this.id = "SatelliteMap";
+				this.spatialReference = new esri.SpatialReference({wkid: 102100});
+				this.initialExtent = (this.fullExtent = new esri.geometry.Extent(13417793.028, 3777430.348, 15084515.335, 4772379.684, this.spatialReference));
+				var lod = [];
+
+				this.tileInfo = me.vworldTileInfo;
+
+				this.loaded = true;
+				this.onLoad(this);
+			},
+			
+			getTileUrl: function (level, row, col) {
+				var returnUrl = "http://xdworld.vworld.kr:8080/2d/Satellite/201301/" + (level) + "/" + col + "/" + row + ".jpeg";
+				return returnUrl;
+			}
+		});
+	},
+	
+	// 브이 월드 중첩맵 세팅
+	initVWorldHybridLayer:function () {
+		var me = this;
+		dojo.declare("VworldTiledHybridMapServiceLayer", esri.layers.TiledMapServiceLayer,{
+			constructor: function () {
+				this.id = "HybridMap";
+				this.spatialReference = new esri.SpatialReference({wkid: 102100});
+				this.initialExtent = (this.fullExtent = new esri.geometry.Extent(13417793.028, 3777430.348, 15084515.335, 4772379.684, this.spatialReference));
+				var lod = [];
+
+				this.tileInfo = me.vworldTileInfo;
+
+				this.loaded = true;
+				this.onLoad(this);
+			},
+			
+			getTileUrl: function (level, row, col) {
+				var returnUrl = "http://xdworld.vworld.kr:8080/2d/Hybrid/201512/" + (level ) + "/" + col + "/" + row + ".png";
+				return returnUrl;
+			}
+		});
 	},
 	
 	extentMove:function(extent, level){
